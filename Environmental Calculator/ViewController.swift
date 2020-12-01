4e24870bb00776c9fda649ec2021eb6fc5873cbb
Corey McLaughlin <coreymclaughlin@coreys-macbook-pro.local>
2020-12-01 10:12:16 -0700

    Initial Commit

---
+++ Environmental Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Environmental Calculator
+//
+//  Created by Corey McLaughlin on 12/1/20.
+//  Copyright © 2020 Corey McLaughlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
